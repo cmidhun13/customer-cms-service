832a6109f9791513f7c5be84fcdcdcd856c0b5e9
Tony Field <tony@tonyfield.ca>
2020-12-15 00:12:48 -0500

    Fix links in HTML sent over from Crafter.

--- src/components/buttons/RenderCrafterComponent.js
+++ src/components/buttons/RenderCrafterComponent.js
@@ -30,10 +30,42 @@ export default function RenderCrafterComponent(props) {
   }
 
   if (crafterComponent) {
+    const html = fixLinks(crafterComponent.markup, site)
     return <section>
-      <div dangerouslySetInnerHTML={{__html: `${crafterComponent.markup}`}}/>
+      <div dangerouslySetInnerHTML={{__html: `${html}`}}/>
     </section>
   }
 
   return <h3>Loading...</h3>
+}
+
+function fixLinks(html, site) {
+  html = fixStaticAssetLinks(html, site)
+  return html
+}
+
+function fixStaticAssetLinks(html, site) {
+  if (html) {
+    let pfx = process.env.REACT_APP_CRAFTER_BASE_URL
+    html = html.replace("/static-assets/", pfx + "/static-assets/")
+
+    // the following section is ONLY required during development, when hostnames are ambiguous.
+    let idx = 0
+    while (idx >= 0) {
+      idx = html.indexOf("/static-assets/", idx);
+      if (idx >= 0) {
+        let endQuote = html.indexOf("\"", idx + 1)
+        if (endQuote >= 0) {
+          let newhtml = html.substr(0, endQuote)
+          newhtml += "?crafterSite=" + site+"\""
+          newhtml += html.substr(endQuote+1);
+          html = newhtml
+          idx = endQuote
+        }
+      }
+    }
+    return html;
+  } else {
+    return html;
+  }
 }
